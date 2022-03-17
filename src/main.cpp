23282c92b798063fbdf13ea1ee1edd325bfe96ba
Danylo Rybchynskyi <d.ribchinski@gmail.com>
2022-03-17 18:37:10 +0200

    Hold the pieces in an int array and print the board Prior to this change, the pieces were stored in a Piece class array. However, this was overcomplicating things.
    This change removes the Piece class and treats all pieces as integers
    stored on the board array. The drawBoard function can now print the
    pieces.

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,34 @@
 #include <iostream>
+int board[8][4];
 
-class Piece {
-   public:
-    const int value;
-    Piece(int c_value)
-        : value(c_value){}
-};
-
-Piece[8][4] board;
-
-startGame() {
+void initBoard() {
     // P1 starting places
     // Queens
-    board[0][2] = Piec(3);
-    board[0][3] = Piece(3);
-    board[1][3] = Piece(3);
+    board[0][2] = 3;
+    board[0][3] = 3;
+    board[1][3] = 3;
     // Drones
-    board[0][1] = Piece(2);
-    board[1][2] = Piece(2);
-    board[2][3] = Piece(2);
+    board[0][1] = 2;
+    board[1][2] = 2;
+    board[2][3] = 2;
     // Pawns
-    board[1][1] = Piece(1);
-    board[2][1] = Piece(1);
-    board[2][2] = Piece(1);
+    board[1][1] = 1;
+    board[2][1] = 1;
+    board[2][2] = 1;
 
     // P2 starting places
     // Queens
-    board[6][0] = Piec(3);
-    board[7][0] = Piece(3);
-    board[7][1] = Piece(3);
+    board[6][0] = 3;
+    board[7][0] = 3;
+    board[7][1] = 3;
     // Drones
-    board[5][0] = Piece(2);
-    board[6][1] = Piece(2);
-    board[7][2] = Piece(2);
+    board[5][0] = 2;
+    board[6][1] = 2;
+    board[7][2] = 2;
     // Pawns
-    board[5][1] = Piece(1);
-    board[5][2] = Piece(1);
-    board[6][2] = Piece(1);
+    board[5][1] = 1;
+    board[5][2] = 1;
+    board[6][2] = 1;
 }
 
 void drawBoard() {
@@ -49,6 +41,8 @@ void drawBoard() {
         else
             std::cout << std::endl;
     }
+    // Draw line separating the ABCD
+    std::cout << "   " << "----------" << std::endl;
     // Draws each row, along with its number
     for (int i = 0; i < 8; ++i) {
         // Draw the dividing line in the middle
@@ -58,17 +52,31 @@ void drawBoard() {
                       << std::endl;
         }
         std::cout << 8 - i << "  ";
-        // print each row of dots
+        // print each row of dots/pieces
         for (int j = 0; j < 4; ++j) {
-            std::cout << '.';
+            switch (board[i][j]) {
+            case 1:
+                std::cout << 'P';
+                break;
+            case 2:
+                std::cout << 'D';
+                break;
+            case 3:
+                std::cout << 'Q';
+                break;
+            default:
+                std::cout << '.';
+            }
             if (j != 3)
                 std::cout << "  ";
         }
         std::cout << std::endl;
     }
+    // Draw line at end of board
+    std::cout << "   " << "----------" << std::endl;
 }
-}
-;
 
 int main() {
+    initBoard();
+    drawBoard();
 }
